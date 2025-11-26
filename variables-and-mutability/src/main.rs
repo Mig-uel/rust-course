dbeeb6b7fc12f1eae0cd27a5e9706986fed44381
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-11-26 13:07:24 -0500

    Add initial Cargo configuration and main function
    - Set up Cargo.toml and Cargo.lock for the project
    - Implement main function with variable inference example

---
+++ variables-and-mutability/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Rust will infer the type of the variable based on the value assigned to it
+    let apples = 50; // i32 is the default type for integer literals
+    // i = integer, 32 = 32 bits
+    // 32 refers to the amount of memory allocated for the variable (32 bits)
+
+    let oranges = 14 + 6; // Rust infers that oranges is also of type i32
+
+    let fruits = apples + oranges; // fruits is also inferred to be i32
+
+    // We can interpolate variables into strings using {}
+    println!("This year, my garden has {apples} apples and {oranges} oranges.");
+}

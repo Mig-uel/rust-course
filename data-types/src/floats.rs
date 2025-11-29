eb6d96f9b1f8f80e3eb3efd4c9aa4f9bd027eaef
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-11-29 13:11:44 -0500

    Add example demonstrating float operations in Rust
    - Introduces a main function showcasing the use of f64 type
    - Displays the value of pi and its floor, ceil, and round methods

---
+++ data-types/src/floats.rs
@@ -0,0 +1,8 @@
+fn main() {
+  let pi: f64 = 3.1415926535897932384;
+  println!("The value of pi is approximately: {}", pi);
+
+  println!("{}", pi.floor());
+  println!("{}", pi.ceil());
+  println!("{}", pi.round());
+}

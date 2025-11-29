06378fafe0234a116d1e0debf654004368b6b264
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-11-29 15:41:50 -0500

    Add example demonstrating character handling in Rust
    - Introduces a simple program to showcase character types
    - Demonstrates usage of `is_uppercase` and `is_alphabetic` methods

---
+++ data-types/src/characters.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let first_initial = 'M';
+    let emoji_face = '😊';
+
+    println!(
+        "{}{}",
+        first_initial.is_uppercase(),
+        emoji_face.is_alphabetic()
+    );
+}

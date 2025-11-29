59c4d25ad68e32150f29500632629fdcfe8c1b77
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-11-29 12:49:12 -0500

    Add usize and isize example for integer types in Rust
    - Introduces a simple example demonstrating the usage of usize
      and isize, highlighting their architecture-dependent sizes.

---
+++ data-types/src/usize-isize.rs
@@ -0,0 +1,7 @@
+fn main() {
+  // usize and isize are nicknames for another exsiting Rust integer type.
+  // Their size depends on the architecture of the computer your program is running on.
+  // In other words, it varies on whether your program is running on a 32-bit or 64-bit system.
+  let x: usize = 10;
+  let y: isize = -5;
+}

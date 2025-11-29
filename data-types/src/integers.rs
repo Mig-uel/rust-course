74c683ea7d5f0825bf0d0ce72c19d13ac9ab130d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-11-29 12:42:53 -0500

    Initialize Cargo configuration and add integers example
    - Create Cargo.toml and Cargo.lock for project setup
    - Add integers.rs with examples of various integer types

---
+++ data-types/src/integers.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let eight_bit_signed: i16 = 32767;
+    let sixteen_bit_unsigned: u16 = 64000;
+
+    let thirty_two_bit_signed = -2147483648;
+    let thirty_two_bit_unsigned: u32 = 4000000000;
+
+    let some_value = 20u8; // Can also use suffix to define type
+}

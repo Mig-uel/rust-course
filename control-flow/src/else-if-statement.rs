6ae469d3c46062f9ed1cbc514879c944acaf68df
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-11-30 17:26:47 -0500

    Add example of else-if statement for control flow Demonstrates the use of else-if statements in Rust to handle multiple conditions based on the season.

---
+++ control-flow/src/else-if-statement.rs
@@ -0,0 +1,13 @@
+fn main() {
+  let season = "summer";
+
+  if season == "summer" {
+    println!("School is out for the summer!");
+  } else if season == "winter" {
+    println!("It's cold outside!");
+  } else if season == "fall" {
+    println!("Leaves are falling!");
+  } else if season == "spring" {
+    println!("Flowers are blooming!");
+  }
+}
